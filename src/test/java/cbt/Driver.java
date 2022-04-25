f2aba9d179705351d4b0a96f947ff8bf1010a136
gabil89 <56522174+gabil89@users.noreply.github.com>
2022-04-25 05:07:15 -0400

    1.chrome-headless 2. false

--- src/test/java/cbt/Driver.java
+++ src/test/java/cbt/Driver.java
@@ -50,7 +50,7 @@ public class Driver {
                     break;
                 case "chrome-headless":
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(false));
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();

4b9f755348ca11f294e5c66eed6bdfec0c4b186f
gabil89 <56522174+gabil89@users.noreply.github.com>
2022-05-08 02:13:44 -0400

    1.chrome-headless 2. false 3.true 4.false 5.true 6.jenkins file 7.jenkins file reopen

--- src/test/java/cbt/CukesRunner.java
+++ src/test/java/cbt/CukesRunner.java
@@ -6,19 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-
-        plugin = {"pretty", "html:target/cucumber.html" ,
-                "rerun:target/rerun.txt" ,  // store the failed scenario into rerun.txt
-                "me.jvt.cucumber.report.PrettyReports:target"  // fancy report
-        } ,
-
-
-
-//        plugin = {
-//                "json:target/cucumber.json",
-//                "html:target/cucumber/report.html",
-//                "junit:target/junit/junit-report.xml",
-//                "rerun:target/rerun.txt",},
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt",},
         features = "src/test/resources/cbt",
         publish = true
 )
